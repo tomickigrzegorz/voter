e37eaf350e8a87fc2f9fb285cd2c72df1525b89c
Grzegorz Tomicki <gtomik@gmail.com>
2021-06-15 17:33:44 +0200

    feat: new button, new process.env

--- sources/script.js
+++ sources/script.js
@@ -36,33 +36,54 @@ function createTitle() {
 
   titleWrapper.insertAdjacentElement('beforeend', buttonGroup);
 
-  showSize(buttonGroup);
-  showSelectedPhoto(buttonGroup);
+  createButtons(buttonGroup);
 }
+
+const sizeButton = document.createElement('button');
 
 function showSize(buttonGroup) {
-  const sizeButton = document.createElement('button');
   sizeButton.className = 'size-button';
-  sizeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M32 4h-4V0h-4v4h-4v4h4v4h4V8h4z"/><path d="M26.996 13.938c.576.64 1.1 1.329 1.563 2.062a15.223 15.223 0 01-4.67 4.697C21.527 22.204 18.799 23 16 23s-5.527-.796-7.889-2.303A15.212 15.212 0 013.441 16a15.223 15.223 0 015.041-4.925A8 8 0 1024 13.812l-.001-.065a8.003 8.003 0 01-5.998-7.635A17.877 17.877 0 0016 5.999c-6.979 0-13.028 4.064-16 10 2.972 5.936 9.021 10 16 10s13.027-4.064 16-10a18.386 18.386 0 00-1.958-3.095 7.952 7.952 0 01-3.046 1.034zM13 10a3 3 0 110 6 3 3 0 010-6z"/></svg>';
+  sizeButton.innerHTML = svgUse('size');
   buttonGroup.insertAdjacentElement('beforeend', sizeButton);
+}
 
-  sizeButton.addEventListener('click', () => {
-    document.body.classList.toggle('show-numbers');
+const showPhotos = document.createElement('button');
+
+function showSelectedPhoto(buttonGroup) {
+  showPhotos.className = 'show-images';
+  showPhotos.innerHTML = svgUse('image');
+  buttonGroup.insertAdjacentElement('beforeend', showPhotos);
+}
+
+function createButtons(buttonGroup) {
+  const buttons = [
+    {
+      name: 'clear-base',
+      svg: 'clear',
+      title: process.env.WYBORNIK_TITLE_CLEAR
+    },
+    {
+      name: 'size-button',
+      svg: 'size',
+      title: process.env.WYBORNIK_TITLE_SHOW_SIZE_IMAGES
+    },
+    {
+      name: 'show-images',
+      svg: 'image',
+      title: process.env.WYBORNIK_TITLE_SHOW_SELECTED_IMAGES
+    }
+  ];
+  buttons.map(({ name, svg, title }) => {
+    let buttonElement = document.createElement('button');
+    buttonElement.classList = name;
+    buttonElement.title = title;
+    buttonElement.innerHTML = svgUse(svg);
+    buttonGroup.insertAdjacentElement('beforeend', buttonElement);
   })
 }
 
-function showSelectedPhoto(buttonGroup) {
-  const showPhotos = document.createElement('button');
-  showPhotos.className = 'show-images';
-  showPhotos.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M12 26h20v4H12zm0-12h20v4H12zm0-12h20v4H12zM6 0v8H4V2H2V0zM4 16.438v1.563h4v2H2v-4.563l4-1.875V12H2v-2h6v4.563zM8 22v10H2v-2h4v-2H2v-2h4v-2H2v-2z"/></svg>';
-  buttonGroup.insertAdjacentElement('beforeend', showPhotos);
-
-  showPhotos.addEventListener('click', () => {
-    const bodyCount = document.body.dataset.count;
-    if (bodyCount > 0) {
-      document.body.classList.toggle('show-all-images');
-    }
-  })
+function svgUse(name) {
+  return `<svg><use xlink:href="#${name}"></use></svg>`;
 }
 
 function allImagesSelected() {
@@ -95,7 +116,6 @@ const template = (image) => {
 
   return temp;
 }
-
 
 
 function addToSiteImages(array) {
@@ -176,11 +196,9 @@ function countElement(count) {
   document.body.dataset.count = count;
 
   showImages.insertAdjacentElement('beforeend', countElement);
-
-  eventHandle();
 }
 
-function eventHandle() {
+(function eventHandle() {
   document.addEventListener('click', (event) => {
     event.preventDefault();
     if (event.target.className === 'img-element') {
@@ -195,8 +213,6 @@ function eventHandle() {
       element.classList.add('active');
       event.target.classList.add('active');
 
-
-
       document.body.classList.remove('show-all-images');
 
       scroll({
@@ -204,11 +220,33 @@ function eventHandle() {
         behavior: "smooth"
       });
     }
+
+    if (event.target.classList.contains('size-button')) {
+      document.body.classList.toggle('show-numbers');
+      event.target.classList.toggle('active');
+    }
+
+    if (event.target.classList.contains('show-images')) {
+      const bodyCount = document.body.dataset.count;
+      if (bodyCount > 0) {
+        document.body.classList.toggle('show-all-images');
+        event.target.classList.toggle('active');
+      }
+    }
+
+    if (event.target.classList.contains('clear-base')) {
+      event.target.classList.toggle('active');
+      if (window.confirm(process.env.WYBORNIK_CLEAR)) {
+        localStorage.clear();
+        location.reload();
+      } else {
+        event.target.classList.remove('active');
+      }
+    }
   })
-}
+})();
 
 function getAllInputsValue() {
-  // let imagesList = '';
   let imagesList = [];
   document.addEventListener('click', (e) => {
     if (e.target.classList.contains('send-email')) {
@@ -239,7 +277,7 @@ function sendEmail(images) {
   const hiddenElement = document.createElement('a');
   hiddenElement.href = encodeURI(csvContent);
   hiddenElement.target = '_blank';
-  hiddenElement.download = 'wszystkie_zdjecia.csv';
+  hiddenElement.download = 'all_images.csv';
   hiddenElement.click();
 }
 
@@ -254,6 +292,6 @@ function setValueNumber() {
 function createButtonSendEmail() {
   const button = document.createElement('button');
   button.className = `send-email`;
-  button.textContent = 'generuj plik ze zdjęciami'
+  button.textContent = process.env.WYBORNIK_GENERATE_CSV
   app.insertAdjacentElement('beforeend', button);
 }
